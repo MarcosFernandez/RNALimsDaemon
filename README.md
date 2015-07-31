dbae7829a52cfff15c8e4b97a9dd28e0e679428c
Marcos Fernandez Callejo <mfernand@cn501.bullx>
2015-07-31 11:52:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+=================
+ RNA Lims Daemon
+=================
+
+RNA Lims Daemon is a GNU/Linux daemon which updates rna seq pipeline results to a Lims Database using REST web services.
+
+Licensing
+=========
+
+RNA Lims Daemon scripts are licensed under GPL.
+
+Installation
+============
+
+Download all scripts and save them at the same folder. 
+Configure the configuration.json according to the documentation.
+
+
+Documentation
+=============
+
+Documentation can be found at [RNA Lims Daemon Documentation] (http://statgen.cnag.cat/daemon_rna_lims/index.html)
+
+
+Changelog:
+==========
+
+    0.0 Initial Release  
+
+
+
---
+++ daemon_rna_lims.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import sys, time
+from daemon import Daemon
+from limsRnaSeq import LimsRnaSeq
+from time import gmtime, strftime
+
+import os,json
+     
+class DaemonRnaLims(Daemon):
+
+    def setConfiguration(self,configuration="None"):
+        """Update Configuration"""
+        self.configuration = configuration
+
+    def run(self):
+        while True:
+            #locate here daemon code
+            self.checkUpdating()
+            time.sleep(14400)
+
+    def checkUpdating(self):
+        """ Reads file of directories to upload to lims """
+        config = {}
+
+        with open(self.configuration) as json_file:
+            config = json.load(json_file)
+
+        logError = []
+
+        file_lims = config["file_lims"]       
+        file_log = config["file_log"]
+        failed_uploads = []
+        
+        if os.path.exists(file_lims):
+            with open(file_lims, "r") as rnaLims:
+                for line in rnaLims:
+                    #1.Create object class LimsRnaSeq File
+                    limsRnaSeq = LimsRnaSeq()
+                    directory = line.strip('\n')
+                    results = limsRnaSeq.run(directory=directory)
+                    if results != "":
+                        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                        logError.append("[%s] Lims updating Error %s"%(time,results))
+                        failed_uploads.append(directory)
+                    else:
+                        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                        logError.append("[%s] Lims updating succesfully executed." %(time))
+
+            if len(logError) != 0:
+                with open(file_log, "a") as log:
+                    for line in logError:
+                        log.write("%s\n" %(line))
+
+            os.remove(file_lims)
+
+            if len(failed_uploads) > 0:
+                with open(file_lims,"w") as fileLims:
+                    for line in failed_uploads:
+                        fileLims.write("%s\n" %(line))
+
+
+     
+if __name__ == "__main__":
+    configuration = os.path.dirname(os.path.realpath(__file__)) + "/configuration.json"
+    daemon = DaemonRnaLims(pidfile='/tmp/daemon-example.pid',stdout="/dev/stdout",stderr="/dev/stderr")
+    daemon.setConfiguration(configuration)
+
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+            sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
+
